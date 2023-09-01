e3ffe493c8d208d2e72d27c801188fcbcaa4abf4
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-01 16:09:49 -0500

    User Service Interface

---
+++ src/main/java/org/ieti/proyecto/service/user/UserService.java
@@ -0,0 +1,15 @@
+package org.ieti.proyecto.service.user;
+
+import org.ieti.proyecto.models.users.User;
+import org.ieti.proyecto.models.users.UserDTO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    public User save(User newUser);
+    Optional<User> findById(String id);
+    List<User> all();
+    void deleteById(String id, User userDelete);
+    User update(UserDTO userUpdates, String userId);
+}
