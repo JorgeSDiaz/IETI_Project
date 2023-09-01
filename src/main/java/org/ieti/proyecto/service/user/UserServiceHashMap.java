d4dd7187bcb65f7b95c2d2270c8bf6773d75f292
Jorge Sz <saenzdiazj0@gmail.com>
2023-09-01 16:09:58 -0500

    User Service Implementation

---
+++ src/main/java/org/ieti/proyecto/service/user/UserServiceHashMap.java
@@ -0,0 +1,48 @@
+package org.ieti.proyecto.service.user;
+
+import org.ieti.proyecto.models.users.User;
+import org.ieti.proyecto.models.users.UserDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class UserServiceHashMap implements UserService {
+    private final Map<String, User> userMap;
+
+    private UserServiceHashMap() {
+        this.userMap = new HashMap<>();
+    }
+    @Override
+    public User save(User newUser) {
+        return this.userMap.put(String.valueOf(this.userMap.size() + 1), newUser);
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(userMap.get(id));
+    }
+
+    private User getUser(String id) {
+        return userMap.get(id);
+    }
+
+    @Override
+    public List<User> all() {
+        return (List<User>) userMap.values();
+    }
+
+    @Override
+    public void deleteById(String id, User userDelete) {
+        userMap.remove(id, userDelete);
+    }
+
+    @Override
+    public User update(UserDTO userUpdates, String userId) {
+        getUser(userId).update(userUpdates);
+        return getUser(userId);
+    }
+}
